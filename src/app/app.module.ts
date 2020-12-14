30d1a247d18154b601e66abfa31c63a65a37930e
sampath kumar <sampathkumar@sampaths-iMac.local>
2020-12-13 20:15:03 -0500

    sample Component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+//Angular uses components to build web pages  and modules to basically bundle different components into packages
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
-  imports: [
+  imports: [ // add other modules to this module
     BrowserModule,
     CommonModule
   ],
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component(
+    {
+        selector : 'app-server', //this can be later used in other component's html file
+        templateUrl:'./server.component.html'
+    }
+)
+export class ServerComponent{
+
+}
